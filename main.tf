c2d83e63949e272b94926c32f258876f632a532c
vick <victordickson474@gmail.com>
2021-10-18 12:00:59 +0100

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "hustler"
+
+    workspaces {
+      name = "hustler-dev"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "us-west-1"
 }
